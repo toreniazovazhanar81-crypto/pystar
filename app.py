e1460860868558988ef9ff5d4b680890ead158c4
toreniazovazhanar81-crypto <toreniazovazhanar81@gmail.com>
2026-09-21 22:31:01 +0500

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ st.markdown("""
 <style>
 
 .stApp {
-     background: linear-gradient(135deg, #16213e, #243b55, #141e30);
+     background: red; linear-gradient(135deg, #16213e, #243b55, #141e30);
     color: #ffffff;
 }
 
